fc3f77a495ae6a0841091ac05631658d6485c913
Alim <122534304+Al1mkaYandere@users.noreply.github.com>
2023-02-01 17:08:15 +0600

    Add files via upload

---
+++ src/main/java/com/example/alimka/Service/PingTask.java
@@ -0,0 +1,37 @@
+package com.example.alimka.Service;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
+@Slf4j
+@Getter
+@Setter
+public class PingTask {
+    @Value("${pingtask.url}")
+    private String url;
+
+    @Scheduled(fixedRateString = "${pingtask.period}")
+    public void pingMe() {
+        try {
+            URL url = new URL(getUrl());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            log.info("Ping {}, OK: response code {}", url.getHost(), connection.getResponseCode());
+            connection.disconnect();
+        } catch (IOException e) {
+            log.error("Ping FAILED");
+            e.printStackTrace();
+        }
+
+    }
+
+}
