7fb01b45fdd24bd6a11a5ac03237cb8c67488290
Alim <122534304+Al1mkaYandere@users.noreply.github.com>
2023-02-01 17:10:38 +0600

    Update AlimkaApplication.java

--- src/main/java/com/example/alimka/AlimkaApplication.java
+++ src/main/java/com/example/alimka/AlimkaApplication.java
@@ -2,8 +2,10 @@ package com.example.alimka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AlimkaApplication {
 
     public static void main(String[] args) {
